1586ec158220d942ab971ab102c2b9c4066ea5ec
Sameirquotb <Sameir.quotb@gmail.com>
2020-11-28 02:57:43 +0200

    Initial commit.

---
+++ src/test/java/com/peopleflw/hr/PeopleflwHrModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peopleflw.hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleflwHrModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
